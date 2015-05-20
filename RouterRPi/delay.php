91e529f9cff6d91924a00fc461088384dc05665e
imoralesgt <ivan.rene.morales@gmail.com>
2015-05-19 19:23:03 -0600

    PHP delay.set modifier
    Now supports on-line sampling period setup though a webpage at
    localhost:80

---
+++ RouterRPi/delay.php
@@ -0,0 +1,43 @@
+<html>
+	<head>
+		<title>
+			Configurar per&iacute;odo de muestreo
+		</title>
+	</head>
+
+	<body>
+
+		<center>
+
+			<?php
+				
+				$min = $_GET['min'];
+				$sec = $_GET['sec'];
+				$ok = 1;
+				$t = 0;
+
+				if ($min >= 0) {
+					$t += ((int) $min) *60;
+				}
+
+				if (($sec > 0) && ($sec < 60)){
+					$t += (int) $sec;
+				}else{
+					$ok = 0;
+				}
+
+				if($ok){
+					$fp = fopen('delay.set', 'w');
+					fwrite($fp, $t);
+					fclose($fp);
+					echo "Nuevo per&iacute;odo establecido: \n<b>".$t."</b> segundos";
+				}else{
+					echo "Formato incorrecto. Retorne para corregir los datos, por favor.";
+				}
+
+			?>
+
+		</center>
+
+	</body>
+</html>
